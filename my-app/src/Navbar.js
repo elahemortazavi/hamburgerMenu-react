6132505069f749285af8e3ce81ecb3e29525130d
elahemortazavi <emor5680@gmail.com>
2023-02-27 23:15:49 +0000

    media query

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -9,7 +9,22 @@ const Ul = styled.ul`
  z-index: 15;
  li{
    padding: 18px 30px;
- }`
+ }
+ @media (max-width: 768px) {
+    margin: 0px;
+    flex-direction: column;
+    background-color: #1565df;
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 300px;
+    height: 100vh;
+    padding-top: 3.5rem;
+    li {
+        color: #fff;
+    }
+ }
+ `;
 
 
 const Navbar = () => {
