0608e406e79f853e124608b38f2acdc1847062fc
elahemortazavi <emor5680@gmail.com>
2023-02-28 13:19:50 +0000

    transition and translation

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -2,33 +2,40 @@ import React from "react";
 import styled from "styled-components";
 
 const Ul = styled.ul`
- list-style: none;
- display: flex;
- justify-content: right;
- margin: 20px 50px;
- z-index: 15;
- li{
-   padding: 18px 30px;
- }
- @media (max-width: 768px) {
+  list-style: none;
+  display: flex;
+  justify-content: right;
+  margin: 20px 50px;
+  z-index: 15;
+
+  li {
+    padding: 18px 30px;
+  }
+  @media (max-width: 768px) {
     margin: 0px;
     flex-direction: column;
     background-color: #1565df;
     position: fixed;
+
+    transition: all 0.3s linear;
+    transform: ${(props) =>
+      props.open ? "translateX(0)" : "translateX(100%)"};
+
     top: 0;
     right: 0;
     width: 300px;
     height: 100vh;
     padding-top: 3.5rem;
     li {
-        color: #fff;
+      color: #000;
     }
- }`;
+  }
+`;
 
 
-const Navbar = () => {
+const Navbar = ({open}) => {
     return (
-      <Ul>
+      <Ul open = {open}>
         <li>Home</li>
         <li>About us</li>
         <li>Docs</li>
